f760e9d65f31efe3061ac2edf45d3e5a981189b5
magdaluyoja <magdaluyoja@gmail.com>
2017-07-16 10:14:05 +0800

    Controllers

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class PagesController extends Controller
+{
+    public function getIndex(){
+    	return view("welcome");
+    }
+    public function getAbout(){
+    	return view("about");
+    }
+}
